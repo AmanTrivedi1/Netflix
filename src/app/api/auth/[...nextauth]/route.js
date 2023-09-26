ce435fa6160b12dfcff8ebaa29dec5d05fd651f9
AmanTrivedi1 <amantrivedi598@gmail.com>
2023-09-26 09:01:37 +0530

    changinf wholecode

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: "05ef1df7b55b43689c11",
+      clientSecret: "7d44ac82ac2836eaabc53a4b178d834a557701f3",
+    }),
+  ],
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.username = session?.user?.name
+        .split(" ")
+        .join("")
+        .toLocaleLowerCase();
+
+      session.user.uid = token.sub;
+
+      return session;
+    },
+  },
+  secret: "default_secret_key",
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
